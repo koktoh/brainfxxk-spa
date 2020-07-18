9bf04b2282d44544dde2f9ac38b12f58d308ac35
koktoh <koktoh96@gmail.com>
2020-07-18 20:27:05 +0900

    modify pages

---
+++ brainfxxk-spa/Models/BrainfxxkExecuter.cs
@@ -0,0 +1,21 @@
+﻿using BFCore.Config;
+using BFCore.Execute;
+
+namespace BrainfxxkSpa.Models
+{
+    public class BrainfxxkExecuter : ExecuterBase
+    {
+        public string Output { get; private set; } = string.Empty;
+
+        public BrainfxxkExecuter(CommonConfig config, BFCommandConfig commandConfig) : base(config, commandConfig) { }
+
+        protected override void Read()
+        {
+        }
+
+        protected override void Write()
+        {
+            this.Output += (char)this._memory[this._index];
+        }
+    }
+}
